3c083c2928caf9588ff56897e114351cd6545ac4
Demian-Moschin <demianmoschin@gmail.com>
2015-09-16 09:04:21 -0300

    [Demian Moschin]- topic 2 - added MovieObserver Class with event handling functions

--- 02-oop-inheritance/javaScript/main.js
+++ 02-oop-inheritance/javaScript/main.js
@@ -1,33 +1,41 @@
+function MovieObserver() {
+
+  this.playing = function (title) {
+    console.log('A movie with title ' + title + ' has been played');
+  } 
+  this.stopped = function(title) {
+    console.log('A movie with title ' + title + ' stopped');
+  } 
+}
 function Movie() {
   this.atributes = new Array();
+  this.observer = new MovieObserver();
 }
-
 Movie.prototype = {
   constructor: Movie,
-
   get:function(atribute) {
      return this.atributes[atribute];
   }, //end get
 
   set:function(atribute, value) {
     this.atributes[atribute] = value;
+      
   },//end set
 
   play:function(){
-    console.log('Playing' + this.atributes['title']);
-    
+    console.log('I am a movie. This is my title: ' + this.atributes['title']);
+    this.observer.playing(this.atributes['title']);
   },//end play
 
   stop:function(){
-    
+    this.observer.stopped(this.atributes['title']);
   }//end stop
 
 
 }// end Movie.prototype
 
 
-
 varMovie = new Movie();
 varMovie.set('title','Pirates of the Sillicon Valley');
-varMovie.get('title');
-
+varMovie.play();
+varMovie.stop();
