ac63081fed39512b5cfcab7fc8a8d033c3679b97
Demian-Moschin <demianmoschin@gmail.com>
2015-10-05 11:06:21 -0300

    [Demian-Moschin]-[07-reactjs] fixed coding error

--- 07-reactjs/flux-react/actions/movie-store-actions.js
+++ 07-reactjs/flux-react/actions/movie-store-actions.js
@@ -1,21 +1,21 @@
 var AppDispatcher = require('./dispatcher/app-dispatcher');
 var AppConstants = require('./constants/app-constants');
 
-var MovieActions = function () {
-};
-MovieActions.prototype.add = function(movie) {
+var MovieActions = function () {};
+
+MovieActions.prototype.add = function (movie) {
   AppDispatcher.handleAction({
     actionType: AppConstants.MOVIE_ADDED,
     movie: movie
   });
 };
-MovieActions.prototype.delete = function(index) {
+MovieActions.prototype.delete = function (index) {
   AppDispatcher.handleAction({
     actionType: AppConstants.MOVIE_DELETED,
     index: index
   });
 };
-MovieActions.prototype.change = function(movie) {
+MovieActions.prototype.change = function (movie) {
 	//TODO IN NEXT COMMIT
 };
 
