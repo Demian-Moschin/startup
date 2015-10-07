e67c40d110bc0308248b048eae8700ce0f32ca74
Demian-Moschin <demianmoschin@gmail.com>
2015-10-07 18:42:52 -0300

    [Demian-Moschin]-[05-AngularJS]- topic 1 and 3. Still working in topic 2

--- 05-angularjs/movie-module/controller/movie.controller.js
+++ 05-angularjs/movie-module/controller/movie.controller.js
@@ -2,13 +2,36 @@
 
   var  appCtrl = angular.module('movie-controller', []);
 
-  appCtrl.controller('MovieController',['$http',function ($http){
-  	var ctrl = this;
+  appCtrl.controller('MovieController',['$scope','$http',function ($scope, $http){
+    var ctrl = this;
     ctrl.movies = [];
+
+    //Let's find out if there's any object in my localStorage. If so, populate movies.
+    if(ctrl.movies.length === 0 && localStorage.length > 0){
+      for(var i = 0 ; i< localStorage.length; i++){
+        var movie = JSON.parse(localStorage.getItem(i));
+        this.movies.push(movie);
+      };
+    };
     
-  	$http.get('./data/movies.json').success(function (data) {
-      ctrl.movies = data;
-  	});
+    //this method will be deprecated. 
+    this.getJSONMovies = function () {
+      $http.get('./data/movies.json').success(function (data) {
+        ctrl.movies = data;
+      });
+    };
+
+    this.addMovie = function () {
+      var newMovie = {title: $scope.movieTitle, director: $scope.movieDirector};
+      this.movies.push(newMovie);
+      localStorage.setItem(this.movies.length-1 ,JSON.stringify(newMovie));
+      $scope.movieTitle = $scope.movieDirector = '';
+    };
+    
+    this.removeMovie = function (index) {
+      this.movies.splice(index, 1);
+      localStorage.removeItem(index);
+    };
 
   }]);
 
