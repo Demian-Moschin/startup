890b38e4510beac04c328e28c4831d566fb144a0
Demian-Moschin <demianmoschin@gmail.com>
2015-10-07 13:09:22 -0300

    [Demian-Moschin]-[05-AngularJS]- Directives changed

--- 05-angularjs/movie-module/directives/movie.directive.js
+++ 05-angularjs/movie-module/directives/movie.directive.js
@@ -2,7 +2,7 @@
 var newMovie = angular.module('newMovie', []).directive('newMovie', function() {
     return {
       restrict: 'E',
-      templateUrl: 'movie-module/views/movie-new.html'
+      templateUrl: 'movie-module/views/movie-new.html',
     };
 });
 var movieList = angular.module('movieList', []).directive('movieList', function() {
@@ -11,5 +11,9 @@ var movieList = angular.module('movieList', []).directive('movieList', function(
       templateUrl: 'movie-module/views/movie-list.html'
     };
 });
-
+var movies = angular.module('movies', []).directive('movies', function() {
+    return {
+      restrict: 'E',
+    };
+});
 })();
